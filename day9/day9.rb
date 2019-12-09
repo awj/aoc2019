43365d0cabbcc3fa467440e33d915436ca713af5
Adam Jones <ajones1@gmail.com>
2019-12-09 14:40:49 -0700

    Day 9 complete.

--- day9/day9.rb
+++ day9/day9.rb
@@ -1,30 +1,19 @@
+load "../intcode.rb"
+
 module Day9
+  module_function
 
-  def self.process_chained_amplifiers(codes, weights)
-    weights.to_a.permutation.map do |config|
-      machines = config.map do |weight|
-        Execution.new(codes, [weight])
+  def run(instructions, flag)
+    ex = Intcode::Execution.new(instructions, [flag])
+
+    out = []
+    loop do
+      ex.process do |output|
+        puts "output: #{output}"
+        out << output
       end
-
-      i = 0
-      machines[0].input << 0
-      final_output = nil
-
-      begin
-        loop do
-          puts "machine: #{i}"
-          machines[i].process do |output|
-            final_output = output
-            i = (i + 1) % machines.size
-            machines[i].input << output
-          end
-        end
-      rescue Halt
-      end
-
-      final_output
-    end.max
+    end
+  rescue Intcode::Halt
+    out
   end
-
-
 end
